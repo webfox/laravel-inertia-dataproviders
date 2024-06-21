ba702886017270c1b4f95dcc3e6220be07e55b36
hailwood <hailwood@users.noreply.github.com>
2024-06-21 02:43:04 +0000

    Fix styling

--- src/AttributeNameFormatters/AsWritten.php
+++ src/AttributeNameFormatters/AsWritten.php
@@ -2,8 +2,6 @@
 
 namespace Webfox\InertiaDataProviders\AttributeNameFormatters;
 
-use Illuminate\Support\Str;
-
 class AsWritten implements AttributeNameFormatter
 {
     public function __invoke(string $name): string
