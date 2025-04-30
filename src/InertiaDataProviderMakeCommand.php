3f18123d4d2283fec9477f1d7fb3768de0047b04
Matthew Hailwood <matthew@webfox.co.nz>
2025-05-01 10:19:56 +1200

    Introduced `make:data-provider` console command. Added support for Laravel Idea.

---
+++ src/InertiaDataProviderMakeCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Webfox\InertiaDataProviders;
+
+use InvalidArgumentException;
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Attribute\AsCommand;
+
+#[AsCommand(name: 'make:data-provider')]
+class InertiaDataProviderMakeCommand extends GeneratorCommand
+{
+    protected $name = 'make:data-provider';
+
+    protected $description = 'Create a new inertia data provider';
+
+    protected $type = 'Inertia Data Provider';
+
+    protected function alreadyExists($rawName)
+    {
+        return class_exists($rawName) ||
+            $this->files->exists($this->getPath($this->qualifyClass($rawName)));
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return $this->resolveStubPath('/stubs/inertia-data-provider.stub');
+    }
+
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : realpath(__DIR__ . '/..' . $stub);
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Http\DataProviders';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the data provider already exists'],
+        ];
+    }
+}
